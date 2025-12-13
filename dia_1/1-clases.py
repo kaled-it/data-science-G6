cd0e8b4894030e29e1de8ffef51133628683a754
kaled-it <icaledlr2@gmail.com>
2025-12-13 11:13:50 -0500

    actualizando dia_4

---
+++ dia_1/1-clases.py
@@ -0,0 +1,16 @@
+#CLASESE EN PYTHON
+# Una clase es una plantilla para crear objetos.
+# Los objetos son instancias de clases.
+# Las clases encapsulan datos y funcionalidades relacionadas.
+# Definición de una clase
+
+class automovil:
+    def __init__(self, aa, pl, col, mar):
+        self.año = aa
+        self.placa = pl
+        self.color = col
+        self.marca = mar
+        
+    del encender(self):
+        print(f'Encendiendo el automóvil {self.marca} con placa {self.placa}')
+        
